510b8743a2c00228676fd028d5e4817aaf012547
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-01-23 16:34:33 +0100

    assemblée selected better

--- app/models/assemblee.rb
+++ app/models/assemblee.rb
@@ -1,5 +1,13 @@
 class Assemblee < ApplicationRecord
   has_many :presences
 
+  after_save :update_fin
+
   scope :ordered, -> { order(début: :desc) }
+
+  private
+
+  def update_fin
+    self.update_column(:fin, self.début + self.durée.to_f.hours)
+  end
 end
