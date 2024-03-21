3b9870bf80ba5ac4bcbc6b80d6aecec58e476736
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-03-21 14:52:12 +0100

    assemblée pdf : images removed / link redirecting to fluo

--- app/pdfs/assemblee_pdf.rb
+++ app/pdfs/assemblee_pdf.rb
@@ -19,43 +19,43 @@ class AssembleePdf
       text "Bonjour #{user.prénom_nom},"
       move_down @margin_down
       text "Vous êtes convoqué à l'assemblée '<b>#{assemblee.nom}</b>' qui aura lieu <b>#{assemblee.adresse}</b>, le <b>#{I18n.l(assemblee.début, format: :long)}</b>", inline_format: true
-      move_down @margin_down * 5
-      text "Voici plusieurs trajets pour y aller depuis chez vous :"
+      # move_down @margin_down * 5
+      # text "Voici plusieurs trajets pour y aller depuis chez vous :"
 
-      move_down @margin_down
-      y_position = cursor
-      bounding_box([0, y_position], :width => 125) do
-        text "En transports", align: :center
-        image "#{@image_path}/trajet.png", :width => 100, position: :center
-        move_down @margin_down
-        text "Émission CO2 : 0.8kg", align: :center
-        text "Durée : 30min", align: :center
-      end
-      bounding_box([130, y_position], :width => 125) do
-        text "À pied", align: :center
-        image "#{@image_path}/trajet.png", :width => 100, position: :center
-        move_down @margin_down
-        text "Émission CO2 : 0g", align: :center
-        text "Durée : 3h", align: :center
-      end
-      bounding_box([260, y_position], :width => 125) do
-        text "En voiture", align: :center
-        image "#{@image_path}/trajet.png", :width => 100, position: :center
-        move_down @margin_down
-        text "Émission CO2 : 6kg", align: :center
-        text "Durée : 20min", align: :center
-      end
+      # move_down @margin_down
+      # y_position = cursor
+      # bounding_box([0, y_position], :width => 125) do
+      #   text "En transports", align: :center
+      #   image "#{@image_path}/trajet.png", :width => 100, position: :center
+      #   move_down @margin_down
+      #   text "Émission CO2 : 0.8kg", align: :center
+      #   text "Durée : 30min", align: :center
+      # end
+      # bounding_box([130, y_position], :width => 125) do
+      #   text "À pied", align: :center
+      #   image "#{@image_path}/trajet.png", :width => 100, position: :center
+      #   move_down @margin_down
+      #   text "Émission CO2 : 0g", align: :center
+      #   text "Durée : 3h", align: :center
+      # end
+      # bounding_box([260, y_position], :width => 125) do
+      #   text "En voiture", align: :center
+      #   image "#{@image_path}/trajet.png", :width => 100, position: :center
+      #   move_down @margin_down
+      #   text "Émission CO2 : 6kg", align: :center
+      #   text "Durée : 20min", align: :center
+      # end
 
-      bounding_box([390, y_position], :width => 125) do
-        text "En voiture", align: :center
-        image "#{@image_path}/trajet.png", :width => 100, position: :center
-        move_down @margin_down
-        text "Émission CO2 : 0g", align: :center
-        text "Durée : 1h", align: :center
-      end
+      # bounding_box([390, y_position], :width => 125) do
+      #   text "En voiture", align: :center
+      #   image "#{@image_path}/trajet.png", :width => 100, position: :center
+      #   move_down @margin_down
+      #   text "Émission CO2 : 0g", align: :center
+      #   text "Durée : 1h", align: :center
+      # end
 
-      move_down @margin_down * 2
-      text "Itinéraires générés par", align: :center
+      move_down @margin_down * 25
+      text "<u><a href='https://www.fluo.eu/'>Voir les itinéraires avec Fluo</a></u>", align: :center, inline_format: true
       image "#{@image_path}/fluo.png", :width => 100, position: :center
     end
   end
