47812de23cb2ffd45de35041e028e2fafc833c07
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-03-22 14:23:06 +0100

    many things

--- app/pdfs/presence_pdf.rb
+++ app/pdfs/presence_pdf.rb
@@ -7,13 +7,17 @@ class PresencePdf
   end
 
   def rapport(presences)
-    presences.each do |presence|
+    text "Feuille d'émargement", size: 36, align: :center
+    text "#{presences.first.assemblee.nom}", size: 36, align: :center
+    move_down @margin_down
+    presences.each_with_index do |presence, index|
+      start_new_page if (index%4 == 3)
       text "#{presence.user.nom_prénom}", size: 20
-      text "#{presence.assemblee.nom.humanize}", size: 16
+      # text "#{presence.assemblee.nom.humanize}", size: 16
       svg Base64.decode64(presence.signature.split(',')[1]), width: 200
       text "#{I18n.l presence.heure, format: :long}"
       move_down @margin_down
-      stroke_horizontal_rule
+      stroke_horizontal_rule unless index%4 == 2
       move_down @margin_down
     end
   end
