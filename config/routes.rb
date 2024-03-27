7c4d721c0ad8a015eb2ebb179bb454ef737b9922
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-03-27 15:22:59 +0100

    PWA

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  root "presences#new"
+  get "/service-worker.js" => "service_worker#service_worker"
+  get "/manifest.json" => "service_worker#manifest"
 end
