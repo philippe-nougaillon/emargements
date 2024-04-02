6f7078efff0973d468a6f5df2b51c0bf8b0d384b
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-04-02 12:19:16 +0200

    email bug fixed / ux

--- app/mailers/assemblee_mailer.rb
+++ app/mailers/assemblee_mailer.rb
@@ -1,6 +1,6 @@
 class AssembleeMailer < ApplicationMailer
   def lien_assemblee(assemblee)
     @assemblee = assemblee
-    mail(to: @assemblee.user.nom, subject: "Émargement de l'assemblée '#{@assemblee.nom}'")
+    mail(to: @assemblee.user.email, subject: "Émargement de l'assemblée '#{@assemblee.nom}'")
   end
 end
