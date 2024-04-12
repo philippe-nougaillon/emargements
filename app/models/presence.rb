b08f69a20f9d664f0a5fe83c29c151652ba8e786
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2024-04-12 18:13:54 +0200

    v2.1 : turbo stream new presences

--- app/models/presence.rb
+++ app/models/presence.rb
@@ -10,9 +10,15 @@ class Presence < ApplicationRecord
 
   scope :ordered, -> { order(heure: :desc) }
 
+  after_create_commit -> { broadcast_prepend_to "presences", 
+                                                partial: "presences/presence", 
+                                                locals: { presence: self }, 
+                                                target: "presences" }
+
   def check_heure
     if !(Time.current > self.assemblee.début && Time.current < (self.assemblee.début + self.assemblee.durée.to_f.hours))
       self.errors.add :erreur, ": Le délai imparti a expiré !" 
     end
   end
+  
 end
