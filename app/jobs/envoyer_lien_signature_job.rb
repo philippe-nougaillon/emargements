ae5811f860217e97e68877e76909eaa038d4f6a6
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-04-18 11:33:08 +0200

    lien_participant fixed

--- app/jobs/envoyer_lien_signature_job.rb
+++ app/jobs/envoyer_lien_signature_job.rb
@@ -2,7 +2,7 @@ class EnvoyerLienSignatureJob < ApplicationJob
   queue_as :default
 
   def perform(assemblee, user)
-    mailer_response = AssembleeMailer.lien_assemblee_participant(assemblee).deliver_now
+    mailer_response = AssembleeMailer.lien_assemblee_participant(assemblee, user).deliver_now
     MailLog.create(organisation_id: assemblee.organisation_id, user_id: 0, message_id: mailer_response.message_id, to: user.email, subject: "Lien signature assemblée")
   end
 end
