30ad826e85637950a67a8c8246c1eb50c3fa0571
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-05-03 11:50:59 +0200

    signature : mobile fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :detect_device_format
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :prepare_exception_notifier
 
   private
+
+  def detect_device_format
+    case request.user_agent
+    when /iPhone/i, /Android/i && /mobile/i, /Windows Phone/i
+      request.variant = :phone
+    else
+      request.variant = :desktop
+    end
+  end
 
   def after_sign_in_path_for(resource)
     resource.admin? ? admin_index_path : super
