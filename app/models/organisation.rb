4c0bc4244f3555a6e47b913cfb7d522604a9588f
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2024-06-07 15:13:11 +0200

    bug fixed

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -28,8 +28,8 @@ class Organisation < ApplicationRecord
   end
 
   def tags
-    assemblees_tags_ids = current_user.organisation.assemblees.tag_counts_on(:tags).pluck(:id)
-    users_tags_ids = current_user.organisation.users.tag_counts_on(:tags).pluck(:id)
+    assemblees_tags_ids = self.assemblees.tag_counts_on(:tags).pluck(:id)
+    users_tags_ids = self.users.tag_counts_on(:tags).pluck(:id)
     return ActsAsTaggableOn::Tag.where(id: assemblees_tags_ids).or(ActsAsTaggableOn::Tag.where(id: users_tags_ids)).order(:name)
   end
 
